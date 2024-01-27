53c6bdd515cfecf540cbd453cd89632f0aeec16a
lemonlux <sololucia17@gmail.com>
2024-01-27 12:22:04 +0100

    data

---
+++ portfolio/src/data/CV.ts
@@ -0,0 +1,125 @@
+export const CV = {
+    person: {
+      name: "Lucía",
+      surname: "Martínez Cerón",
+      image: "https://res.cloudinary.com/daxddugwt/image/upload/v1705932775/perfil_copia_rlbbdo.png" ,
+      CVpdf: "https://drive.google.com/file/d/1txOeBZ14_o-u2K566rrPbGqYuymm89w1/view?usp=sharing",
+      brief: "FullStack Developer | Graduada en Química",
+      location: "Murcia, España",
+      email: "lucia.mceron@gmail.com",
+      birthDate: "17/02/1996",
+      gitHub: "https://github.com/lemonlux/",
+      linkedin:"https://www.linkedin.com/in/luc%C3%ADa-mart%C3%ADnez-cer%C3%B3n-7326b2185/",
+      aboutMe: [
+         "Siempre me ha interesado mucho la informática, tanto que, aunque estudié química, estuve 2 años como alumna interna durante la carrera desarrollando un proyecto de química computacional, en el que más tarde basé mi TFG.", 
+         "Después de la carrera, hice un Máster en Ciencia e Industria cosmética y empecé trabajando como Analista de Laboratorio, puesto el cual he desempeñado durante 4 años, hasta que por fin me decidí a seguir formándome en lo que realmente me interesa: la programación web.",
+         "Actualmente me encuentro buscando un trabajo que me brinde la oportunidad de adentrarme laboralmente en el sector del web development, mientras desarrollo proyectos por mi cuenta y sigo formándome en las tecnologías que me interesan.",
+         "Como curiosidad, me encanta dedicar mi tiempo libre a la ilustración digital, un hobbie que tengo desde hace años."
+      ],
+      tech: "Trabajo con varias tecnologías y metodologías, aunque me encuentro en continuo aprendizaje.",
+      plusInfo: "Actualmente estudiando: ",
+    },
+    education: [
+      {
+        name: "Bootcamp Full Stack Developer",
+        id: 3,
+        date: "sept 2023 - dic 2023",
+        where: "NEOLAND",
+        description:
+          "Curso intensivo de desarrollo web en el que adquirí conocimientos de Backend y Frontend, sobre los lenguajes Javascript, HTML y CSS, y tecnologías como React, Node.js, MongoDB, Styled Components, Vite, GitHub, Axios,... "
+      },
+      {
+        name: "Máster en Industria Cosmética",
+        id: 2,
+        date: "feb 2019 - jun 2020",
+        where: "Colegio de Químicos de Murcia",
+        description:
+          ""
+      },
+      {
+        name: "Grado en Química",
+        id: 1,
+        date: "2014 - 2019",
+        where: "Universidad de Murcia",
+        description:
+        "Alumna interna durante 2 años, desarrollando un proyecto de Química Computacional sobre el que más tarde basé mi TFG"
+      },
+    ],
+    experience: [
+      {
+        name: "Química en laboratorio cromatográfico",
+        date: "nov 2019 - nov 2023",
+        where: "Iberchem",
+        description:
+          "Análisis químico, cromatografía de gases, control de calidad. SAP, AMDIS y Agilent Data Analysis.",
+      },
+      {
+        name: "Freelance",
+        date: "sept 2023 - actualidad",
+        where: "",
+        description:
+          "Desde el inicio del Bootcamp de Web Development, he desarrollado diversos proyectos tanto por mi cuenta como en equipo con el resto de alumnos del curso.",
+      }
+    ],
+    projects: [
+      {
+        name: "Connect-a-mate",
+        date: "dic 2023",
+        image: "https://res.cloudinary.com/daxddugwt/image/upload/v1705060049/Captura_de_pantalla_2024-01-11_a_las_14.40.53_legrz1.png",
+        description:
+          "Proyecto final de bootcamp que realicé en equipo junto a mis compañeros de curso. Se trata de una red social para conectar y buscar personas afines a ti a la hora de compartir piso. Cuenta con autenticación de usuario y personalización del perfil, publicaciones en un feed, reseñas a los usuarios, chat privado, así como los anuncios de cada habitación disponible.",
+        technologies: ["Javascript", "Node.js", "MongoDB", "React", "Styled Components"],
+        url: "https://connect-a-mate.vercel.app/",
+        frontEnd: "https://github.com/neolandsep2023/Frontend",
+        backEnd: "https://github.com/neolandsep2023/Backend",
+      },
+      {
+        name: "Sports",
+        date: "nov 2023",
+        image: "https://res.cloudinary.com/daxddugwt/image/upload/v1705060048/Captura_de_pantalla_2024-01-11_a_las_12.27.09_wrsint.png",
+        description:
+          'Proyecto de React + Node.js que realicé con mis compañeros. Es una página de varios deportes en la que practicamos todos los conceptos aprendidos de React, utilizando Node.js para las llamadas a la base de datos, y sobre la que cabe destacar la posibilidad de los usuarios de crear sus "podios" de motogp y "eleven" de fútbol favoritos.',
+        technologies: ["Javascript", "Node.js", "MongoDB", "React", "HTML", "CSS"],
+        url: "https://fifa-sports.vercel.app/",
+        frontEnd:  "https://github.com/eolimacion/reactFrontend",
+        backEnd: "https://github.com/eolimacion/reactBackend",
+      },
+ 
+    ],
+    languages: [
+        {
+         language: "Español",
+        level: "Nativo",
+        },
+        {
+      language: "Inglés",
+      level: "C1",
+
+    }],
+    hardSkills: [
+      "HTML5",
+      "CSS3",
+      "Javascript",
+      "React",
+      "Styled Components",
+      "Nodejs",
+      "Insomnia",
+      "MongoDB",
+      "Vite",
+      "axios",
+      "SCRUM",
+    
+    ],
+    softSkills: [
+        "Creatividad",
+        "Trabajo en equipo",
+        "Organización",
+        "Análisis",
+        "Iniciativa",
+        "Adaptación",
+        "Resolución de problemas",
+        "Empatía",
+        "Comunicación y escucha"
+    ]
+  
+  };
